b9a5d46880fe61f105bdb28ec3ae317bcb58ac73
James Chan <jameschanx@gmail.com>
2018-08-06 22:35:58 -0700

    nice result, refine interval and improve visualization next, maybe use 1 more dataset

--- main.py
+++ main.py
@@ -67,32 +67,54 @@ if __name__=="__main__":
     X_wine_scaled = scale(X_wine)
     Y_wine = pd.read_hdf('y_wine.hdf')
     
-    X_train, X_test, Y_train, Y_test = train_test_split(X_abalone_scaled, Y_abalone, test_size = .2)
-    X_train, X_prune, Y_train, Y_prune = train_test_split(X_train, Y_train, test_size = .25)
-    
-    training_sizes = [i for i in range(10,110,10)]
+    abalone_test_scores = []
+    abalone_pruned_scores = []
+    wine_test_scores = []
+    wine_pruned_scores = []
+    training_sizes = [i for i in range(1,105,15)]
     for ts in training_sizes:
-        print(int(ts/100 * X_train.shape[0]))
-        X_batch = X_train[:int(ts/100 * X_train.shape[0])]
-        Y_batch = Y_train[:int(ts/100 * Y_train.shape[0])]
-    
-        #evaluate pruning in Decision Tree with Abalone
-        dt = DTC()
-        dt.fit(X_batch, Y_batch)
-        training_score = dt.score(X_batch, Y_batch)
-        testing_score = dt.score(X_test, Y_test)
+        print(ts)
+        abalone_acc = []
+        abalone_acc_pruned = []
+        wine_acc = []
+        wine_acc_pruned = []
+        #average of 5 trials
+        for i in range(10):
+            print('----',i)
+            X_train, X_test, Y_train, Y_test = train_test_split(X_abalone_scaled, Y_abalone, test_size = .2)
+            X_train, X_prune, Y_train, Y_prune = train_test_split(X_train, Y_train, test_size = .25)
+            X_batch = X_train[:int(ts/100 * X_train.shape[0])]
+            Y_batch = Y_train[:int(ts/100 * Y_train.shape[0])]    
         
-        dt = error_reduction_pruning(dt, X_prune, Y_prune)
-        post_pruning_score = dt.score(X_test, Y_test)
-        print(ts, training_score, testing_score, post_pruning_score)
+            #evaluate pruning in Decision Tree with Abalone
+            dt = DTC()
+            dt.fit(X_batch, Y_batch)
+            testing_score = dt.score(X_test, Y_test)
+            dt = error_reduction_pruning(dt, X_prune, Y_prune)
+            post_pruning_score = dt.score(X_test, Y_test)
+            abalone_acc.append(testing_score)
+            abalone_acc_pruned.append(post_pruning_score)
+            
+            #evaluate pruning in Decision Tree with Wine
+            dt = DTC()
+            dt.fit(X_batch, Y_batch)
+            testing_score = dt.score(X_test, Y_test)
         
-        #evaluate pruning in Decision Tree with Wine
-        dt = DTC()
-        dt.fit(X_batch, Y_batch)
-        training_score = dt.score(X_batch, Y_batch)
-        testing_score = dt.score(X_test, Y_test)
+            dt = error_reduction_pruning(dt, X_prune, Y_prune)
+            post_pruning_score = dt.score(X_test, Y_test)
+            wine_acc.append(testing_score)
+            wine_acc_pruned.append(post_pruning_score)
+        
+        #average scores
+        abalone_test_scores.append(np.mean(abalone_acc))
+        abalone_pruned_scores.append(np.mean(abalone_acc_pruned))
+        wine_test_scores.append(np.mean(wine_acc))
+        wine_pruned_scores.append(np.mean(wine_acc_pruned))
     
-        dt = error_reduction_pruning(dt, X_prune, Y_prune)
-        post_pruning_score = dt.score(X_test, Y_test)
-        print(ts, training_score, testing_score, post_pruning_score)
-        print
+    plt.figure()
+    plt.plot(training_sizes, abalone_test_scores)
+    plt.plot(training_sizes, abalone_pruned_scores)
+    plt.figure()
+    plt.plot(training_sizes, wine_test_scores)
+    plt.plot(training_sizes, wine_pruned_scores)
+        
